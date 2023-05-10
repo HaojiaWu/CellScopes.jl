47bd4faecdb38311a01cc69711d5d0eda78da322
HaojiaWu <haojiawu@wustl.edu>
2023-05-10 17:29:38 -0500

    updates

--- src/scrna/fileio.jl
+++ src/scrna/fileio.jl
@@ -139,6 +139,7 @@ function read_xenium(xenium_dir::String; prefix = "xenium", min_gene::Int64 = 0,
     count_molecules.cell = string.(count_molecules.cell)
     count_molecules = filter(:gene => ∈(Set(genes2)), count_molecules)
     count_cells.cell = string.(count_cells.cell)
+    count_cells.cluster = clustering.Cluster
     spObj = XeniumObject(count_molecules, count_cells, raw_count, poly, umap_obj;
             prefix = prefix, min_gene = min_gene, min_cell = min_gene)
     clustering.cell = prefix .*  "_" .* string.(clustering.Barcode)
