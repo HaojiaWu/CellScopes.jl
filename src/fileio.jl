19a69fdc16e62144e3102611e2bbc3b8ea0e9fa6
home_server <haojiawu@wustl.edu>
2024-04-17 21:48:58 -0500

    updates

--- src/fileio.jl
+++ src/fileio.jl
@@ -17,17 +17,21 @@ function read_10x(tenx_dir::String;
     else
         error("version can only be v2 or v3!")
     end
-    cells = string.(cells.Column1)
-    genes = string.(genes.Column2)
-    gene_kept = (vec ∘ collect)(rowSum(counts).> min_cell)
-    genes = genes[gene_kept]
-    cell_kept = (vec ∘ collect)(colSum(counts) .> min_gene)
-    cells = cells[cell_kept]
-    counts = counts[gene_kept, cell_kept]
-    gene_kept, gene_removed = check_duplicates(genes)
-    gene_removed = collect(values(gene_removed))
-    counts = counts[Not(gene_removed), :]
-    rawcount = RawCountObject(counts, cells, gene_kept)
+    if min_gene > 0 || min_cell > 0
+        cells = string.(cells.Column1)
+        genes = string.(genes.Column2)
+        gene_kept = (vec ∘ collect)(rowSum(counts).> min_cell)
+        genes = genes[gene_kept]
+        cell_kept = (vec ∘ collect)(colSum(counts) .> min_gene)
+        cells = cells[cell_kept]
+        counts = counts[gene_kept, cell_kept]
+        gene_kept, gene_removed = check_duplicates(genes)
+        gene_removed = collect(values(gene_removed))
+        counts = counts[Not(gene_removed), :]
+        rawcount = RawCountObject(counts, cells, gene_kept)
+    else
+        rawcount = RawCountObject(counts, cells, genes)
+    end
     return rawcount
 end
 
